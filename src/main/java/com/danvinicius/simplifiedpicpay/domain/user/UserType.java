8132941f94ce7fac6dc4ccbdb89f027d052bc767
Daniel Vinícius <viniccius774@gmail.com>
2024-04-28 17:29:02 -0300

    feat: first commit and first entities

---
+++ src/main/java/com/danvinicius/simplifiedpicpay/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package com.danvinicius.simplifiedpicpay.domain.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
---
+++ src/test/java/com/danvinicius/simplifiedpicpay/SimplifiedPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danvinicius.simplifiedpicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
